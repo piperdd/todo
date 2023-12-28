103d1bbe43898a35995cdf5bcd1301b5c1c85f55
piperdd <harunazh2003@gmail.com>
2023-12-28 22:41:09 +0000

    added checkboxes + labels

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ var cardList = document.querySelector(".card-list");
 
 document.querySelector(".add-button").addEventListener("click", ()=>{
     var newCard = document.createElement("div");
-    newCard.appendChild(document.createTextNode("Hello"));
+    var checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    // checkBox.name = "hello";
+    var label = document.createElement("label");
+    label.htmlFor = "id";
+    label.appendChild(document.createTextNode("hello"));
+    newCard.appendChild(checkBox);
+    newCard.appendChild(label);
     cardList.appendChild(newCard);
 })
 
