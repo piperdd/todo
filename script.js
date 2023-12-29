3a4bd1e16a3a4e08af136f87caebe9426f92de64
piperdd <harunazh2003@gmail.com>
2023-12-29 21:02:07 +0000

    modified add card button to be dynamic

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var card = document.querySelector(".card");
 var cardList = document.querySelector(".card-list");
+
 document.querySelector(".add-button").addEventListener("click", ()=>{
     var newCard = document.createElement("div");
     newCard.classList.add("card");
@@ -18,47 +19,81 @@ document.querySelector(".add-button").addEventListener("click", ()=>{
     cardList.appendChild(newCard);
 })
 
-var todoList = document.querySelector(".todo-list");
-
-document.querySelector(".add-item").addEventListener("click", ()=>{
-    var newItem = document.createElement("div");
-    newItem.classList.add('todo')
-    //add checkBox
-    var checkBox = document.createElement("input");
-    checkBox.type = "checkbox";
-    checkBox.classList.add('todo-check')
-    //label (not used)
-    var label = document.createElement("label");
-    label.htmlFor = "id";
-    //add textbox
-    var textBox = document.createElement("input");
-    textBox.type = "text";
-    textBox.classList.add('todo-desc')
-    //add remove button
-    var removeButton = document.createElement("button");
-    removeButton.textContent = "X";
-    removeButton.className = "remove-button"
-    label.appendChild(document.createTextNode("hello"));
-    newItem.appendChild(checkBox);
-    newItem.appendChild(textBox);
-    newItem.appendChild(removeButton);
-    todoList.appendChild(newItem);
-    // cursor automatically selects new text box
-    textBox.focus();
-    textBox.select();
-})
-
-todoList.addEventListener("click", (e) => {
+cardList.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("add-item")){
+        var todoList = e.target.parentElement;
+        var newItem = document.createElement("div");
+        newItem.classList.add('todo')
+        //add checkBox
+        var checkBox = document.createElement("input");
+        checkBox.type = "checkbox";
+        checkBox.classList.add('todo-check')
+        //label (not used)
+        var label = document.createElement("label");
+        label.htmlFor = "id";
+        //add textbox
+        var textBox = document.createElement("input");
+        textBox.type = "text";
+        textBox.classList.add('todo-desc')
+        //add remove button
+        var removeButton = document.createElement("button");
+        removeButton.textContent = "X";
+        removeButton.className = "remove-button"
+        label.appendChild(document.createTextNode("hello"));
+        newItem.appendChild(checkBox);
+        newItem.appendChild(textBox);
+        newItem.appendChild(removeButton);
+        todoList.appendChild(newItem);
+        // cursor automatically selects new text box
+        textBox.focus();
+        textBox.select();
+    }
     if(e.target.classList.contains("remove-button")){
         e.target.parentElement.remove();
     }
 });
 
-todoList.addEventListener("click", (e)=>{
-    if(e.target.classList.contains("todo-check")){
-        var clone = e.target.parentElement;
-        clone.classList.add('todo-done')
+// var todoList = document.querySelector(".todo-list");
 
-        // document.querySelector(".done-list").appendChild(clone);
-    }
-})
+// document.querySelector(".add-item").addEventListener("click", ()=>{
+//     var newItem = document.createElement("div");
+//     newItem.classList.add('todo')
+//     //add checkBox
+//     var checkBox = document.createElement("input");
+//     checkBox.type = "checkbox";
+//     checkBox.classList.add('todo-check')
+//     //label (not used)
+//     var label = document.createElement("label");
+//     label.htmlFor = "id";
+//     //add textbox
+//     var textBox = document.createElement("input");
+//     textBox.type = "text";
+//     textBox.classList.add('todo-desc')
+//     //add remove button
+//     var removeButton = document.createElement("button");
+//     removeButton.textContent = "X";
+//     removeButton.className = "remove-button"
+//     label.appendChild(document.createTextNode("hello"));
+//     newItem.appendChild(checkBox);
+//     newItem.appendChild(textBox);
+//     newItem.appendChild(removeButton);
+//     todoList.appendChild(newItem);
+//     // cursor automatically selects new text box
+//     textBox.focus();
+//     textBox.select();
+// })
+
+// todoList.addEventListener("click", (e) => {
+//     if(e.target.classList.contains("remove-button")){
+//         e.target.parentElement.remove();
+//     }
+// });
+
+// todoList.addEventListener("click", (e)=>{
+//     if(e.target.classList.contains("todo-check")){
+//         var clone = e.target.parentElement;
+//         clone.classList.add('todo-done')
+
+//         // document.querySelector(".done-list").appendChild(clone);
+//     }
+// });
